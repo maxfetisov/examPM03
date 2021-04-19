8aba9ba1ad5a0f5f31d4ba7b6d82b3a4eaab4476
qwe <maksimka.fetisov.02@mail.ru>
2021-04-19 13:19:26 +0300

    Добавлен метод заполнения массива

--- src/src/Program.cs
+++ src/src/Program.cs
@@ -10,6 +10,62 @@ namespace src
     {
         static void Main(string[] args)
         {
+            int n;
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Введите количество элементов в массиве: ");
+                    n = Convert.ToInt32(Console.ReadLine());
+                    break;
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine("Введено некорректное значение: " + ex.Message);
+                }
+            }
+            App[] apps = new App[n];
+        }
+        private void fillApps(App[] apps)
+        {
+            for(int i = 0; i < apps.Length; i++)
+            {
+                Console.WriteLine($"Введите наименование программы {i+1}: ");
+                string stmp = Console.ReadLine();
+                if (!string.IsNullOrEmpty(stmp))
+                {
+                    apps[i].Name = stmp;
+                }
+                else
+                {
+                    Console.WriteLine("Введена пустая строка");
+                    i--;
+                    continue;
+                }
+                Console.WriteLine($"Введите производителя программы {i + 1}: ");
+                stmp = Console.ReadLine();
+                if (!string.IsNullOrEmpty(stmp))
+                {
+                    apps[i].Producer = stmp;
+                }
+                else
+                {
+                    Console.WriteLine("Введена пустая строка");
+                    i--;
+                    continue;
+                }
+                Console.WriteLine($"Введите цену программы {i + 1}: ");
+                try
+                {
+                    apps[i].Price = Convert.ToDouble(Console.ReadLine());
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine("Введено некорректное значение: " + ex.Message);
+                    i--;
+                    continue;
+                }
+            }
         }
     }
 }
