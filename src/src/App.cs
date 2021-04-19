dac450bead94d5cfb00f99ad6cee09306bf53070
qwe <maksimka.fetisov.02@mail.ru>
2021-04-19 13:01:10 +0300

    Добавлен и описан класс App

---
+++ src/src/App.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace src
+{
+    public class App
+    {
+        private string name;
+        private string producer;
+        private double price;
+
+        public string Name { get => name; set => name = value; }
+        public string Producer { get => producer; set => producer = value; }
+        public double Price { get => price; set => price = value; }
+
+        public App() { }
+        public App(string name, string producer, double price)
+        {
+            this.name = name;
+            this.producer = producer;
+            this.price = price;
+        }
+
+        public override string ToString() => $"{name}, {producer}, {price}";
+    }
+}
